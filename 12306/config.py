e141b6e88b2f64b0ebb57cb0be89b24f5d01387d
venmsu <31070458+venmsu@users.noreply.github.com>
2018-03-18 10:18:20 +0800

    Update config.py
    把个人的账号密码去掉了。

--- 12306/config.py
+++ 12306/config.py
@@ -1,7 +1,7 @@
 # 购票信息填写
 
-coUsername = "CH19940902"
-coPassword = "yy246437"
+coUsername = "12306账号"
+coPassword = "12306账号密码"
 
 # ====购买车票的消息====
 
@@ -15,17 +15,19 @@ coToStation = "上海"
 coDate = "2018-03-22"
 # 座位类型
 coSeatType = "硬座"
-# 座位位置，动车和高铁才能选座，其余不得选座，
+# 座位位置，动车和高铁才能选座，其余不得选座，输入如（1A）
 coSeat = ""
-coName = "金顺伟"
+coName = "你的姓名"
 
 # 成人票填写ADULT,学生票填写0X00
 coPuerpose = "ADULT"
 # 单程票填写dc。往返票填写wc
 coTourflag = "dc"
-coId = "330326199309203656"
-coPhonenum = "15267032557"
-# 邮件接受账号
-receiveMail = "862054822@qq.com"
+# 乘车人身份证
+coId = "xxxxxxxxxxxxxxxxxx"
+# 乘车人电话号码
+coPhonenum = "138xxxxxxxx"
+# 邮件接受账号，有的邮箱通知可能会在垃圾邮件中，建议使用QQ邮箱
+receiveMail = "xxxxx@qq.com"
 
 
